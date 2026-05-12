bedf2353f2200fa82b7c5bf451aa8dfa5c0c56e0
Ingo Ruck <i.ruck.b@gmail.com>
2026-05-12 18:04:39 +0200

    chore: infra config, typo changes

--- infra/config.ts
+++ infra/config.ts
@@ -1,3 +1,3 @@
-export const APP_ID = 'digital-business-card-v1'; // app identifier string, what is the aapp about
-export const GITHUB_USER_NAME = 'iru-b'; //
-export const GITHUB_REPO_NAME = 'digital-business-card';
+export const APP_ID = "html-template"; // app identifier string, what is the aapp about
+export const GITHUB_USER_NAME = "iru-b"; //
+export const GITHUB_REPO_NAME = "html-template-vite-aws-tooling";
