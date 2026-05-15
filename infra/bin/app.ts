6e4fe1baefa75f98016723390d378a60bf925486
Ingo Ruck <i.ruck.b@gmail.com>
2026-05-15 13:14:17 +0200

    chore: use custom domain (#8)

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -1,22 +1,30 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { FrontendInfraStack } from '../lib/frontend-infra-stack';
-import { GitHubDeployStack } from '../lib/github-deploy-stack';
-import { APP_ID, GITHUB_REPO_NAME, GITHUB_USER_NAME } from '../config';
-import { Props } from '../lib/types';
+import * as cdk from "aws-cdk-lib";
+import {
+	APP_ID,
+	AWS_CERTIFICATE_ARN,
+	CUSTOM_DOMAIN,
+	GITHUB_REPO_NAME,
+	GITHUB_USER_NAME,
+} from "../config";
+import { FrontendInfraStack } from "../lib/frontend-infra-stack";
+import { GitHubDeployStack } from "../lib/github-deploy-stack";
+import type { Props } from "../lib/types";
 
 const app = new cdk.App();
 
 // 🌍 Production only (no staging)
 const env: Props = {
-    githubUser: GITHUB_USER_NAME,
-    githubRepo: GITHUB_REPO_NAME
+	githubUser: GITHUB_USER_NAME,
+	githubRepo: GITHUB_REPO_NAME,
+	customDomainName: CUSTOM_DOMAIN,
+	certificateArn: AWS_CERTIFICATE_ARN,
 };
 
 const infra = new FrontendInfraStack(app, APP_ID, env);
 
-new GitHubDeployStack(app, 'GitHubDeployStack', {
-    ...env,
-    bucketName: infra.bucket.bucketName,
-    distributionId: infra.distribution.distributionId,
+new GitHubDeployStack(app, "GitHubDeployStack", {
+	...env,
+	bucketName: infra.bucket.bucketName,
+	distributionId: infra.distribution.distributionId,
 });
--- infra/lib/frontend-infra-stack.ts
+++ infra/lib/frontend-infra-stack.ts
@@ -1,53 +1,67 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
-import { Props } from './types';
+import * as cdk from "aws-cdk-lib";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import type { Construct } from "constructs";
+import type { Props } from "./types";
 
 export class FrontendInfraStack extends cdk.Stack {
-    public readonly bucket: s3.Bucket;
-    public readonly distribution: cloudfront.Distribution;
+	public readonly bucket: s3.Bucket;
+	public readonly distribution: cloudfront.Distribution;
 
-    constructor(scope: Construct, id: string, props?: Props) {
-        super(scope, id, props);
+	constructor(scope: Construct, id: string, props: Props) {
+		super(scope, id, props);
 
-        // 🪣 Private S3 bucket (NO public access)
-        this.bucket = new s3.Bucket(this, `${id}-bucket`, {
-            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-            removalPolicy: cdk.RemovalPolicy.RETAIN,
-        });
+		// 🪣 Private S3 bucket (NO public access)
+		this.bucket = new s3.Bucket(this, `${id}-bucket`, {
+			blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+			removalPolicy: cdk.RemovalPolicy.RETAIN,
+		});
 
-        // 🌍 CloudFront (modern OAC setup)
-        this.distribution = new cloudfront.Distribution(this, `${id}-cdn`, {
-            defaultRootObject: 'index.html',
+		// 🔐 ACM certificate (must be us-east-1 for CloudFront)
+		const certificate = acm.Certificate.fromCertificateArn(
+			this,
+			"Cert",
+			props.certificateArn,
+		);
 
-            defaultBehavior: {
-                origin: origins.S3BucketOrigin.withOriginAccessControl(this.bucket),
-                viewerProtocolPolicy:
-                    cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            },
+		// 🌍 CloudFront (modern OAC setup)
+		this.distribution = new cloudfront.Distribution(this, `${id}-cdn`, {
+			defaultRootObject: "index.html",
+			domainNames: [props.customDomainName],
 
-            // 🧠 SPA routing support (important for Vite/React) - remove comments if needed
-            //   errorResponses: [
-            //     {
-            //       httpStatus: 404,
-            //       responseHttpStatus: 200,
-            //       responsePagePath: '/index.html',
-            //     },
-            //   ],
-        });
+			certificate,
 
-        new cdk.CfnOutput(this, 'BucketName', {
-            value: this.bucket.bucketName,
-        });
+			defaultBehavior: {
+				origin: origins.S3BucketOrigin.withOriginAccessControl(this.bucket),
+				viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+			},
 
-        new cdk.CfnOutput(this, 'DistributionId', {
-            value: this.distribution.distributionId,
-        });
+			// 🧠 SPA routing support (important for Vite/React) - remove comments if needed
+			//   errorResponses: [
+			//     {
+			//       httpStatus: 404,
+			//       responseHttpStatus: 200,
+			//       responsePagePath: '/index.html',
+			//     },
+			//   ],
+		});
 
-        new cdk.CfnOutput(this, 'CloudFrontURL', {
-            value: this.distribution.distributionDomainName,
-        });
-    }
+		new cdk.CfnOutput(this, "BucketName", {
+			value: this.bucket.bucketName,
+		});
+
+		new cdk.CfnOutput(this, "DistributionId", {
+			value: this.distribution.distributionId,
+		});
+
+		new cdk.CfnOutput(this, "CloudFrontURL", {
+			value: this.distribution.distributionDomainName,
+		});
+
+		new cdk.CfnOutput(this, "CustomDomain", {
+			value: `https://${props.customDomainName}`,
+		});
+	}
 }
